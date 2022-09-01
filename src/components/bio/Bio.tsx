a428f7bf118cfd560b8f51c8f544a1c513003a3d
Jamie Hewitt <jamiehewitt@protonmail.com>
2022-09-01 01:40:41 +0100

    bio updated

--- src/components/bio/Bio.tsx
+++ src/components/bio/Bio.tsx
@@ -6,24 +6,23 @@ import CircleMe from 'assets/circleme.png';
 function Bio() {
   return (
     <section id="about" className="bio">
-      <div>
+      <div className="bio-container">
         <h2>Full Stack Web Developer</h2>
         <h4>
           HTML • CSS • Javascript • Node.js • ES6+ • React • Redux • GraphQL •
           Typescript • MongoDB • Git
         </h4>
         <p>
-          A web developer adept in building responsive, well-designed web
-          applications, front and back end. My training with The Jump Digital
-          School has prepared me to build professional and efficient
-          applications suited to the needs of the clients.
+          A web developer adept in building responsive, well-designed web applications, from front to back end.
+          <span> My training with <a href="https://www.thejump.tech">The Jump: Digital School </a> 
+          has prepared me to build professional and efficient applications
+          suited to the needs of my clients.</span>
         </p>
         <h3>Who am I?</h3>
         <img height="400" src={CircleMe} alt="A Picture of Jamie Hewitt" />
         <p>
-          I am currently studying at&nbsp;
-          <a href="https://www.thejump.tech">The Jump: Coding Bootcamp</a> in
-          hopes of a career change to web development. Currently a barista and
+          I am currently training in
+          hopes of a career change to web development. While I am currently a barista and
           coffee lover, I am looking for a new challenge in an exciting
           industry. My skills and experience are varied, I have an eye for
           details that is needed within the coffee industry, and expereience
